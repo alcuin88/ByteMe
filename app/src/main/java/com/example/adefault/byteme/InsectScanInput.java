0f28a06c736fba99d848733f2f54dc7f37b4758d
alcuin88 <alcuin88@gmail.com>
2019-01-11 13:11:27 +0800

    My first commit

--- app/src/main/java/com/example/adefault/byteme/InsectScanInput.java
+++ app/src/main/java/com/example/adefault/byteme/InsectScanInput.java
@@ -1,13 +1,39 @@
 package com.example.adefault.byteme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class InsectScanInput extends AppCompatActivity {
+
+    Button scanInsect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insect_scan_input);
+
+        refIDs();
+
+        scanInsect.setOnClickListener(ClickListener);
+    }
+
+    private OnClickListener ClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.scanInsectButton:
+                    Intent scanInsectIntent = new Intent(view.getContext(), InsectScan.class);
+                    startActivity(scanInsectIntent);
+                    break;
+            }
+        }
+    };
+
+    private void refIDs(){
+        scanInsect = findViewById(R.id.scanInsectButton);
     }
 }
--- app/src/main/java/com/example/adefault/byteme/ScanModeForm.java
+++ app/src/main/java/com/example/adefault/byteme/ScanModeForm.java
@@ -1,13 +1,54 @@
 package com.example.adefault.byteme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ScanModeForm extends AppCompatActivity {
+
+    Button byBite, byInsect, back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_mode_form);
+
+        refIDs();
+        byBite.setOnClickListener(ClickListener);
+        byInsect.setOnClickListener(ClickListener);
+        back.setOnClickListener(ClickListener);
+    }
+
+    private OnClickListener ClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()){
+                case R.id.byBiteButton:
+                    Intent biteScanIntent = new Intent(view.getContext(), BiteScanInput.class);
+                    startActivity(biteScanIntent);
+                    break;
+
+                case R.id.byInsectButton:
+                    Intent insectScanIntent = new Intent(view.getContext(), InsectScanInput.class);
+                    startActivity(insectScanIntent);
+                    break;
+
+                case R.id.backButton:
+                    Intent backIntent = new Intent(view.getContext(), Dashboard.class);
+                    backIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(backIntent);
+                    finish();
+                    break;
+            }
+        }
+    };
+
+    private void refIDs(){
+        byBite = findViewById(R.id.byBiteButton);
+        byInsect = findViewById(R.id.byInsectButton);
+        back = findViewById(R.id.backButton);
     }
 }
